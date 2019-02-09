6bb516615bffd1e8d0a0c11d3ecb5cdc37356594
amitrappel <33310717+amitrappel@users.noreply.github.com>
2019-02-09 21:43:24 +0200

    Update birthday.py

--- my_package/birthday.py
+++ my_package/birthday.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
 class BirthDay:
-    def __init__(year, month, day):
+    def __init__(self, year, month, day):
         self.bday = datetime(year, month, day)     
 
-    def get_weekday():
+    def get_weekday(self):
         return self.bday.weekday()
     
-    def get_age(self)
+    def get_age(self):
         return datetime.now() - self.bday
